c6b9cc58e582ec396c80d1ee78c267684ee11391
Anthony Urbina <aurbinava002@gmail.com>
2022-08-10 17:59:59 +0000

    completed prep-javascript-functions

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+function getGreeting(name) {
+  return 'Hello, ' + name + '!';
+}
+
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+
+function getCircleCircumference(radius) {
+  return 2 * Math.PI * radius;
+}
+
+function getFullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+
+function cube(number) {
+  return number * number * number;
+}
